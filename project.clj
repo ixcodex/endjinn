823b32dd604b6806d2a2cd2f7e292ed8c2b726fe
Jim Barritt <jim@planet-ix.com>
2013-07-25 12:13:49 +0100

    [jmdb] Fixing monger dependency hopefully

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-yaml "0.4.0"]
+                 [com.novemberain/monger "1.4.1"]
                  ]
 )
